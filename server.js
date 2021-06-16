0bbf57aac2deb7e4d2d1e787ca447a185678f4de
BarbaraSheaKenny <b.kenny58@gmail.com>
2021-06-15 20:58:50 -0400

    server.js fix

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+
 
 const app = express();
 const PORT = process.env.PORT || 3008;
@@ -21,20 +23,17 @@ const sess = {
 };
 
 app.use(session(sess));
- app.get("/", function(req, res) {
-      res.json(path.join(__dirname, "public/index.html"));
-  });
-  
+ 
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
